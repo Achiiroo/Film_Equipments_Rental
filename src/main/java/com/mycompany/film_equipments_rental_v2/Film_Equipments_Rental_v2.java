68451c86674ce78f90c453c1fc3b34f9fabc8fcb
Achiiroo <johndavealdaybriones009@gmail.com>
2024-11-13 12:31:06 +0800

    Film equipment Login-SignUp

---
+++ src/main/java/com/mycompany/film_equipments_rental_v2/Film_Equipments_Rental_v2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.film_equipments_rental_v2;
+
+/**
+ *
+ * @author dave
+ */
+public class Film_Equipments_Rental_v2 {
+
+    public static void main(String[] args) {
+        Login MainMenu = new Login();
+        MainMenu.setVisible(true);
+        MainMenu.pack();
+        MainMenu.setLocationRelativeTo(null);
+    }
+}
