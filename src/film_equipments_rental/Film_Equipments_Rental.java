f563faa2d3b25375dbdbf157f3ef410c00b6ce94
Achiiroo <johndavealdaybriones009@gmail.com>
2024-11-14 16:39:40 +0800

    Re-upload Files

---
+++ src/film_equipments_rental/Film_Equipments_Rental.java
@@ -0,0 +1,16 @@
+
+package film_equipments_rental;
+import UI.Login;
+
+/**
+ *
+ * @author dave
+ */
+public class Film_Equipments_Rental {
+    public static void main(String[] args) {
+        Login MainMenu = new Login();
+        MainMenu.setVisible(true);
+        MainMenu.pack();
+        MainMenu.setLocationRelativeTo(null);
+        }
+}
